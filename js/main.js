47b43f7bdada225ff0ed51c476e0ab17eec127f4
Marina <takasi.taku.97@list.ru>
2023-03-08 21:37:42 +1000

    делает тз "больше деталей"

---
+++ js/main.js
@@ -0,0 +1,90 @@
+// массив из 25 сгенерированных объектов.
+// каждый объект массива — описание фотографии, опубликованной пользователем.
+const descriptionsArray = [
+  'Дом',
+  'Природа',
+  'Цветы',
+  'Солнце',
+  'Котики',
+  'Машинки',
+  'Комната',
+  'Работа',
+  'Музей',
+  'Отдых'
+];
+
+// массив объектов — список комментариев,
+//  оставленных другими пользователями к этой фотографии.
+const messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  `Когда вы делаете фотографию, хорошо бы убирать палец из кадра.
+   В конце концов это просто непрофессионально.`,
+  `Моя бабушка случайно чихнула с фотоаппаратом в руках
+   и у неё получилась фотография лучше.`,
+  `Я поскользнулся на банановой кожуре и уронил фотоаппарат
+   на кота и у меня получилась фотография лучше.`,
+  `Лица у людей на фотке перекошены, как будто их избивают.
+   Как можно было поймать такой неудачный момент?!`
+];
+
+const names = [
+  'Артур',
+  'Ибрагим',
+  'Валера',
+  'Андрей',
+  'Юля',
+  'Юлий',
+  'Ольга',
+  'Олег',
+  'Илья'
+];
+
+// функция для получения рандомного положительного числа
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+// функция для получения рандомного описания фотограии
+const getRandomDescription = (descriptions) => {
+  let result = '';
+  for (let i = 0; i < getRandomPositiveInteger(2, 5); i++) {
+    result += `${descriptions[getRandomPositiveInteger(0, descriptions.length - 1)] } `;
+  }
+  return result.trim();
+};
+
+// функция для создания массива с комментариями.
+const getComment = () => {
+  const result = [];
+  for (let i = 0; i < getRandomPositiveInteger(1, 3); i++) {
+    const commentsUsers = {
+      id: getRandomPositiveInteger(1, 300),
+      avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+      message: messages[getRandomPositiveInteger(1, messages.length - 1)],
+      name: names[getRandomPositiveInteger(1, names.length - 1)]
+    };
+    result.push(commentsUsers);
+  }
+  return result;
+};
+
+// функция для создания массива с фотографиями
+const getArrayObject = () => {
+  const result = [];
+  for (let i = 1; i <= 25; i++) {
+    const photoUser = {
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: getRandomDescription(descriptionsArray),
+      likes: getRandomPositiveInteger(15, 200),
+      comments: getComment()
+    };
+    result.push(photoUser);
+  }
+  return result;
+};
+getArrayObject();
